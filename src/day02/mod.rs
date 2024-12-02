56e25f1e1befdd48df54b2bc310e25ca904355da
Maciej Jedwabny <rudykot@gmail.com>
2024-12-02 16:48:37 +0100

    day02 part 2

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -7,6 +7,29 @@ pub fn count_safe_reports(input: &mut dyn BufRead) -> usize {
   reports.iter().filter(|r| is_report_safe(r)).count()
 }
 
+pub fn count_safe_reports_with_toleration(input: &mut dyn BufRead) -> usize {
+  let reports = parse_input(read_input(input));
+  reports.iter().filter(|r| is_report_safe_with_toleration(r)).count()
+}
+
+fn is_report_safe_with_toleration(report: &Vec<u32>) -> bool {
+  is_report_safe(report)
+    || (0..report.len()).any(|level: usize| is_report_safe_without_level(report, level))
+}
+
+fn is_report_safe_without_level(report: &Vec<u32>, level: usize) -> bool {
+  fn without_level(report: &Vec<u32>, level: usize) -> Vec<u32> {
+    report
+      .iter()
+      .enumerate()
+      .filter(|&(i, _)| i != level)
+      .map(|(_, v)| *v)
+      .collect::<Vec<u32>>()
+  }
+
+  is_report_safe(&without_level(report, level))
+}
+
 fn is_report_safe(report: &Vec<u32>) -> bool {
   are_levels_monothonic(report, |d| d >= 1 && d <= 3)
     || are_levels_monothonic(report, |d| d <= -1 && d >= -3)
@@ -35,7 +58,7 @@ fn parse_input(input: Vec<String>) -> Vec<Vec<u32>> {
 
 #[cfg(test)]
 mod tests {
-  use crate::day02::count_safe_reports;
+  use crate::day02::{count_safe_reports, count_safe_reports_with_toleration};
   use std::{fs::File, io::BufReader};
 
   #[test]
@@ -48,5 +71,17 @@ mod tests {
   fn my_part1_input() {
     let mut f = BufReader::new(File::open("./src/day02/my.input").unwrap());
     assert_eq!(count_safe_reports(&mut f), 390);
+  }
+
+  #[test]
+  fn sample_part2_input() {
+    let mut f = BufReader::new(File::open("./src/day02/sample.input").unwrap());
+    assert_eq!(count_safe_reports_with_toleration(&mut f), 4);
+  }
+
+  #[test]
+  fn my_part2_input() {
+    let mut f = BufReader::new(File::open("./src/day02/my.input").unwrap());
+    assert_eq!(count_safe_reports_with_toleration(&mut f), 439);
   }
 }
