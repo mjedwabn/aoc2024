7b3d0b6d36d12de7e8a838152c1610cbfa8ca4b1
Maciej Jedwabny <rudykot@gmail.com>
2024-12-03 08:21:28 +0100

    day03 part 2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -15,9 +15,40 @@ fn extract_valid_muls(code: String) -> Vec<(u32, u32)> {
     .collect::<Vec<(u32, u32)>>()
 }
 
+pub fn sum_enabled_multiplications(input: &mut dyn BufRead) -> u32 {
+  let muls = extract_enabled_muls(read_input(input).concat());
+  muls.iter().map(|(a, b)| a * b).sum()
+}
+
+fn extract_enabled_muls(code: String) -> Vec<(u32, u32)> {
+  let re = regex::Regex::new(r"(?<disable>don't\(\))|(?<enable>do\(\))|mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)").unwrap();
+  
+  let mut enabled = true;
+  let mut enabled_muls: Vec<(u32, u32)> = Vec::new();
+  
+  for x in re.captures_iter(&code) {
+    if x.name("disable").is_some() {
+      enabled = false;
+    }
+    else if x.name("enable").is_some() {
+      enabled = true;
+    }
+    else {
+      if enabled {
+        enabled_muls.push((
+          x.name("a").unwrap().as_str().parse::<u32>().unwrap(), 
+          x.name("b").unwrap().as_str().parse::<u32>().unwrap()
+        ))
+      }
+    }
+  }
+
+  enabled_muls
+}
+
 #[cfg(test)]
 mod tests {
-  use crate::day03::sum_multiplications;
+  use crate::day03::{sum_enabled_multiplications, sum_multiplications};
   use std::{fs::File, io::BufReader};
 
   #[test]
@@ -30,5 +61,16 @@ mod tests {
   fn my_part1_input() {
     let mut f = BufReader::new(File::open("./src/day03/my.input").unwrap());
     assert_eq!(sum_multiplications(&mut f), 174960292)
+  }
+
+  #[test]
+  fn sample_part2_input() {
+    assert_eq!(sum_enabled_multiplications(&mut "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".as_bytes()), 48)
+  }
+
+  #[test]
+  fn my_part2_input() {
+    let mut f = BufReader::new(File::open("./src/day03/my.input").unwrap());
+    assert_eq!(sum_enabled_multiplications(&mut f), 56275602)
   }
 }
