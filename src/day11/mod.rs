1b8415bf8d3decb811e1ddaf427969866d2d4018
Maciej Jedwabny <rudykot@gmail.com>
2024-12-12 23:44:16 +0100

    day11 part 2

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,50 +1,72 @@
-use std::io::BufRead;
+use std::{collections::HashMap, io::BufRead};
+
+use itertools::Itertools;
 
 use crate::read_input;
 
 pub fn count_stones(input: &mut dyn BufRead, blinks: u32) -> usize {
-  let mut stones: Vec<u64> = parse_stones(read_input(input));
+  let mut stones: HashMap<u64, usize> = parse_stones(read_input(input));
 
   for _ in 0..blinks {
-    let mut i = 0;
-    
-    while i < stones.len() {
-      let stone = stones[i];
+    let mut tmp: HashMap<u64, isize> = HashMap::new();
 
-      if stone == 0 {
-        stones[i] = 1;
+    for (stone, n) in stones.iter() {
+      let (left, right) = if *stone == 0 {
+        (1, None)
       } else {
         let s = stone.to_string();
         if s.len() % 2 == 0 {
           let (left, right) = s.split_at(s.len() / 2);
-          let left_stone = left.parse::<u64>().unwrap();
-          let right_stone = right.parse::<u64>().unwrap();
-
-          stones[i] = left_stone;
-
-          if i == stones.len() - 1 {
-            stones.push(right_stone);
-          } else {
-            stones.insert(i + 1, right_stone);
-          }
-
-          i += 1;
+          (
+            left.parse::<u64>().unwrap(),
+            Some(right.parse::<u64>().unwrap()),
+          )
         } else {
-          stones[i] = stones[i] * 2024;
+          (stone * 2024, None)
         }
-      }
+      };
 
-      i += 1;
+      tmp
+        .entry(*stone)
+        .and_modify(|count| *count = *count - *n as isize)
+        .or_insert(-(*n as isize));
+      
+      tmp
+        .entry(left)
+        .and_modify(|count| *count = *count + *n as isize)
+        .or_insert(*n as isize);
+      
+      if let Some(r) = right {
+        tmp
+          .entry(r)
+          .and_modify(|count| *count = *count + *n as isize)
+          .or_insert(*n as isize);
+      }
+    }
+
+    for (stone, n) in tmp {
+      stones
+        .entry(stone)
+        .and_modify(|count| *count = (*count as isize + n) as usize)
+        .or_insert(n as usize);
     }
   }
 
-  stones.len()
+  stones.values().sum()
 }
 
-fn parse_stones(lines: Vec<String>) -> Vec<u64> {
+fn parse_stones(lines: Vec<String>) -> HashMap<u64, usize> {
   lines
     .first()
-    .map(|line| line.split(' ').flat_map(|s| s.parse::<u64>()).collect())
+    .map(|line| {
+      line
+        .split(' ')
+        .flat_map(|s| s.parse::<u64>())
+        .into_group_map_by(|&s| s)
+        .into_iter()
+        .map(|s| (s.0, s.1.len()))
+        .collect()
+    })
     .unwrap()
 }
 
@@ -54,20 +76,24 @@ mod tests {
 
   #[test]
   fn sample1_part1_input() {
-    assert_eq!(count_stones(&mut read("./src/day11/sample1.input"), 1), 7)
+    assert_eq!(count_stones(&mut read("./src/day11/sample1.input"), 1), 7);
   }
 
   #[test]
   fn sample2_part1_input() {
     assert_eq!(count_stones(&mut read("./src/day11/sample2.input"), 6), 22);
-    assert_eq!(
-      count_stones(&mut read("./src/day11/sample2.input"), 25),
-      55312
-    )
   }
 
   #[test]
   fn my_part1_input() {
-    assert_eq!(count_stones(&mut read("./src/day11/my.input"), 25), 197357)
+    assert_eq!(count_stones(&mut read("./src/day11/my.input"), 25), 197357);
+  }
+
+  #[test]
+  fn my_part2_input() {
+    assert_eq!(
+      count_stones(&mut read("./src/day11/my.input"), 75),
+      234568186890978
+    )
   }
 }
